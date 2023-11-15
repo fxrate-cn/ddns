48897ca980455e734123fe6e77230a668c75a6c5
Wynston Hou <houyewei@gmail.com>
2023-11-15 22:35:55 +0800

    update request

--- ddns.go
+++ ddns.go
@@ -97,11 +97,31 @@ type Record struct {
 
 func setDNS(ip string) {
 	zoneId := goDotEnvVariable("zoneId")
-	apiToken := goDotEnvVariable("apiToken")
 	name := goDotEnvVariable("name")
 	baseUrl := "https://api.cloudflare.com/client/v4/zones/"
 	dnsRecordsApi := fmt.Sprint(baseUrl, zoneId, "/dns_records")
-	req, err := http.NewRequest("GET", dnsRecordsApi, nil)
+	bodyBytes := request("GET", dnsRecordsApi, nil)
+	var result DNSRecords
+	if err := json.Unmarshal(bodyBytes, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+	var id = ""
+	// Loop through the data and find the record id of domain name
+	for _, rec := range result.Result {
+		if rec.Name == name {
+			id = rec.ID
+		}
+	}
+	updateApi := dnsRecordsApi + "/" + id
+	record := &Record{Name: name, Content: ip, Proxied: true}
+	jsonValue, _ := json.Marshal(record)
+	updateResult := request("PATCH", updateApi, bytes.NewBuffer(jsonValue))
+	fmt.Println(string(updateResult))
+}
+
+func request(method string, url string, reader io.Reader) []byte {
+	apiToken := goDotEnvVariable("apiToken")
+	req, err := http.NewRequest(method, url, reader)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -109,46 +129,16 @@ func setDNS(ip string) {
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		var result DNSRecords
-		if err := json.Unmarshal(bodyBytes, &result); err != nil { // Parse []byte to the go struct pointer
-			fmt.Println("Can not unmarshal JSON")
-		}
-
-		var id = ""
-		// Loop through the data node for the FirstName
-		for _, rec := range result.Result {
-			if rec.Name == name {
-				id = rec.ID
-			}
-		}
-		updateApi := dnsRecordsApi + "/" + id
-		record := Record{}
-		record.Name = name
-		record.Content = ip
-		record.Proxied = true
-		jsonValue, _ := json.Marshal(record)
-		req, err := http.NewRequest("PATCH", updateApi, bytes.NewBuffer(jsonValue))
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Header.Set("Authorization", "Bearer "+apiToken)
-		req.Header.Set("Content-Type", "application/json")
-		resp, err := client.Do(req)
-		defer resp.Body.Close()
-		if resp.StatusCode == http.StatusOK {
-			bodyBytes, err := io.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(string(bodyBytes))
-		}
-
+		return bodyBytes
 	}
+	return nil
 }
